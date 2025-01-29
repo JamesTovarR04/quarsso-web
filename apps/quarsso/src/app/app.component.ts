1321f635eb64cd1713d62ceff950898fb261f54b
JamesTovarR04 <jamestovar09@hotmail.com>
2025-01-28 23:30:21 -0500

    se agrega material y scss de plantilla

--- apps/quarsso/src/app/app.component.ts
+++ apps/quarsso/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, MatToolbarModule, MatIconModule, MatButtonModule],
   selector: 'qss-root',
   templateUrl: './app.component.html',
 })
