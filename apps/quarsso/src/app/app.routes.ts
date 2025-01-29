1f85e5f9fc2234c44f9e0b7f588485a5a4c89b1b
JamesTovarR04 <jamestovar09@hotmail.com>
2025-01-28 22:11:34 -0500

    se agrega proyecto budget

--- apps/quarsso/src/app/app.routes.ts
+++ apps/quarsso/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'budget',
+    loadChildren: () => import('budget/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
     path: '',
     component: NxWelcomeComponent,
   },
