b9ce150f9bf72056e4622daa34f65537114b920e
JamesTovarR04 <jamestovar09@hotmail.com>
2025-01-29 19:29:07 -0500

    se remueve proyecto budget

--- apps/budget/src/app/pages/pages.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Route } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { ReportsComponent } from './reports/reports.component';
-
-export const remoteRoutes: Route[] = [
-  { path: '', title: 'Start', component: NxWelcomeComponent },
-  { path: 'reports', title: 'Reports', component: ReportsComponent },
-];
